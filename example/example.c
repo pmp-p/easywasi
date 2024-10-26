866a51bef626d6564af3dfc8eb77c940a02b84a8
David Konsumer <konsumer@jetboystudio.com>
2024-10-25 20:45:35 -0700

    better tester

--- example/example.c
+++ example/example.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <unistd.h>
 
 typedef unsigned char rand_type;
 rand_type my_rand() {
@@ -17,14 +18,13 @@ int print_file(char* filename) {
   char ch;
   file_ptr = fopen(filename, "r");
   if (NULL == file_ptr) {
-    fprintf(stderr, "file can't be opened \n");
+    fprintf(stderr, "file can't be opened: %s\n", filename);
     return 1;
   }
   printf("CONTENTS OF %s:\n", filename);
   fflush(stdout);
   while ((ch = fgetc(file_ptr)) != EOF) {
     printf("%c", ch);
-    fflush(stdout);
   }
   fclose(file_ptr);
   return 0;
@@ -34,28 +34,42 @@ int main(int argc, char *argv[]) {
   printf("hello. argv works: %d\n", argc);
   for (int i=0;i<argc;i++) {
     printf("  %d: %s\n", i, argv[i]);
-    fflush(stdout);
   }
+  fflush(stdout);
 
   printf("env works:\n");
   const char* COOL = getenv("COOL");
   printf("  COOL=%s\n", COOL);
+  fflush(stdout);
 
   printf("stdout works.\n");
   fflush(stdout);
   fprintf(stderr, "stderr works.\n");
+  fflush(stderr);
 
   time_t rawtime;
-  time ( &rawtime );
-  printf ( "time works: %s", ctime(&rawtime));
+  struct tm *info;
+  time(&rawtime);
+  info = localtime(&rawtime);
+  printf("time works: %s", asctime(info));
   fflush(stdout);
 
   srand(time(NULL));
   printf("random works: %hhu\n", my_rand());
   fflush(stdout);
 
-  // I can read files from zip
-  print_file("/mnt/zip/cyber.txt");
+  printf ("I can read files from zip:\n");
+  fflush(stdout);
+  char* filename = "/zip/cyber.txt";
+  if (access(filename, F_OK) == 0) {
+    printf("file exists: %s\n", filename);
+    print_file(filename);
+    fflush(stdout);
+    return 1;
+  } else {
+    fprintf(stderr, "file does not exist: %s\n", filename);
+    fflush(stderr);
+  }
 
   return 0;
 }
