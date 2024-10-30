0e1e8f3e2b26414062827513383635a67027c671
David Konsumer <konsumer@jetboystudio.com>
2024-10-29 18:17:04 -0700

    stub FSDummy

--- docs/easywasi.js
+++ docs/easywasi.js
@@ -9,7 +9,72 @@ export class WASIProcExit extends Error {
   }
 }
 
-export class FSDummy {}
+// FS interface that is used here. Implement your own, if you want!
+export class FSDummy {
+  appendFileSync (path, data, options = {}) {
+    throw new Error('appendFileSync not implemented. This is a dummy fs.')
+  }
+
+  fsyncSync (fd) {
+    throw new Error('fsyncSync not implemented. This is a dummy fs.')
+  }
+
+  linkSync (existingPath, newPath) {
+    throw new Error('linkSync not implemented. This is a dummy fs.')
+  }
+
+  mkdirSync (path, options = {}) {
+    throw new Error('mkdirSync not implemented. This is a dummy fs.')
+  }
+
+  readdirSync (path, options = {}) {
+    throw new Error('readdirSync not implemented. This is a dummy fs.')
+  }
+
+  readFileSync (path, options = {}) {
+    throw new Error('readFileSync not implemented. This is a dummy fs.')
+  }
+
+  readlinkSync (path, options = {}) {
+    throw new Error('readlinkSync not implemented. This is a dummy fs.')
+  }
+
+  renameSync (oldPath, newPath) {
+    throw new Error('renameSync not implemented. This is a dummy fs.')
+  }
+
+  rmdirSync (path, options = {}) {
+    throw new Error('rmdirSync not implemented. This is a dummy fs.')
+  }
+
+  setFlagsSync (path, flags) {
+    throw new Error('setFlagsSync not implemented. This is a dummy fs.')
+  }
+
+  statSync (path, options = {}) {
+    throw new Error('statSync not implemented. This is a dummy fs.')
+  }
+
+  symlinkSync (target, path, type = 'file') {
+    throw new Error('symlinkSync not implemented. This is a dummy fs.')
+  }
+
+  truncateSync (path, len = 0) {
+    throw new Error('truncateSync not implemented. This is a dummy fs.')
+  }
+
+  unlinkSync (path) {
+    throw new Error('unlinkSync not implemented. This is a dummy fs.')
+  }
+
+  utimesSync (path, atime, mtime) {
+    throw new Error('utimesSync not implemented. This is a dummy fs.')
+  }
+
+  writeFileSync (path, data, options = {}) {
+    throw new Error('writeFileSync not implemented. This is a dummy fs.')
+  }
+}
 
 export class WasiPreview1 {
   constructor (options = {}) {
@@ -359,7 +424,7 @@ export class WasiPreview1 {
       }
 
       // Verify file exists
-      const stats = this.fs.statSync(resolvedPath)
+      this.fs.statSync(resolvedPath)
 
       // Store path and initial position in handle
       const fd = this.allocateFd({ path: resolvedPath, position: 0 }, 'file')
